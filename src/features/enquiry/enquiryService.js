d54c8ee090e39f870148a06de966cd292ecca020
Rubie02 <hoainn9a@gmail.com>
2023-10-29 12:59:54 +0700

    connect blogs, coupons, colors, categories

--- src/features/enquiry/enquiryService.js
+++ src/features/enquiry/enquiryService.js
@@ -2,21 +2,21 @@ import axios from "axios";
 import { config } from "../../utils/axiosconfig";
 import { base_url } from "../../utils/baseUrl";
 const getEnquiries = async () => {
-  const response = await axios.get(`${base_url}enquiry/`);
+  const response = await axios.get(`${base_url}enquiries/`, config);
 
   return response.data;
 };
 const deleteEnquiry = async (id) => {
-  const response = await axios.delete(`${base_url}enquiry/${id}`, config);
+  const response = await axios.delete(`${base_url}enquiries/${id}`, config);
   return response.data;
 };
 const getEnquiry = async (id) => {
-  const response = await axios.get(`${base_url}enquiry/${id}`);
+  const response = await axios.get(`${base_url}enquiries/${id}`);
   return response.data;
 };
 const udpateEnquiry = async (enq) => {
   const response = await axios.put(
-    `${base_url}enquiry/${enq.id}`,
+    `${base_url}enquiries/${enq.id}`,
     { status: enq.enqData },
     config
   );
--- src/features/pcategory/pcategoryService.js
+++ src/features/pcategory/pcategoryService.js
@@ -3,31 +3,31 @@ import { base_url } from "../../utils/baseUrl";
 import { config } from "../../utils/axiosconfig";
 
 const getProductCategories = async () => {
-  const response = await axios.get(`${base_url}category/`);
+  const response = await axios.get(`${base_url}categories/`);
 
   return response.data;
 };
 const createCategory = async (category) => {
-  const response = await axios.post(`${base_url}category/`, category, config);
+  const response = await axios.post(`${base_url}categories/`, category, config);
 
   return response.data;
 };
 
 const getProductCategory = async (id) => {
-  const response = await axios.get(`${base_url}category/${id}`, config);
+  const response = await axios.get(`${base_url}categories/${id}`, config);
 
   return response.data;
 };
 
 const deleteProductCategory = async (id) => {
-  const response = await axios.delete(`${base_url}category/${id}`, config);
+  const response = await axios.delete(`${base_url}categories/${id}`, config);
 
   return response.data;
 };
 const updateProductCategory = async (category) => {
   console.log(category);
   const response = await axios.put(
-    `${base_url}category/${category.id}`,
+    `${base_url}categories/${category.id}`,
     { title: category.pCatData.title },
     config
   );
